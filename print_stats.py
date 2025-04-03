0410591a30cf891f1b83b48267fa56caada2883f
Ryan1284 <ryan12810@gmail.com>
2025-04-02 19:48:09 -0500

    Test

---
+++ print_stats.py
@@ -0,0 +1,4 @@
+import stats
+
+# Print all attributes and methods available in the stats module
+print(dir(stats))
--- stats.py
+++ stats.py
@@ -3,3 +3,24 @@ print("Stats module loaded!")
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def char_count(text):
+    char_dict = {}
+    for ind_char in text:
+        char = ind_char.lower() 
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
